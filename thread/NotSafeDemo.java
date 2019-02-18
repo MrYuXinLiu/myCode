a4e1a72afbff3ead595856c56012c919b2fa47ef
刘新宇 <lxy980504@foxmail.com>
2019-02-18 19:29:43 +0800

    mycode first Thread

---
+++ thread/NotSafeDemo.java
@@ -0,0 +1,55 @@
+package com.example.thread;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class NotSafeDemo {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new ConcurrentHashMap<>();
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 4));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+
+    }
+
+
+    public static void setNotSafe() {
+
+        Set<String> hashSet = new CopyOnWriteArraySet<>();
+
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                hashSet.add(UUID.randomUUID().toString().substring(0, 4));
+                System.out.println(hashSet);
+
+            }, String.valueOf(i)).start();
+        }
+
+    }
+
+
+    public static void listNotSafe() {
+        //判断几个线程是不安全的
+
+        List<String> list = new CopyOnWriteArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+
+                list.add(UUID.randomUUID().toString().substring(0, 4));
+                System.out.println(list);
+
+            }, String.valueOf(i)).start();
+
+        }
+
+    }
+
+
+}
---
+++ thread/Test.java
@@ -0,0 +1,52 @@
+package com.example.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Test {
+
+
+    //多线程
+    // 1. 几个线程   操作如何    资源库
+    public static void main(String[] args) {
+        // 先创建一个资源库
+        resource resource = new resource();
+        // 开启三个线程
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) resource.save();
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) resource.save();
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 0; i < 40; i++) resource.save();
+        }, "c").start();
+
+        new Thread(() -> {
+            for (int i = 0; i <2; i++) resource.save(); ;
+        }, "线程名字").start();
+    }
+
+
+}
+
+class resource {
+    //1 . 售票员的数量
+    private int personCount = 3;
+    //2 . 一共多少张票
+    private int ticket = 30;
+    //3 . 避免并发,所以创建一个Lock;
+    private Lock lock = new ReentrantLock();
+
+    public void save() {
+        lock.lock();
+        try {
+            if (ticket > 0) {
+                System.out.println(Thread.currentThread().getName() + "售出第" + ticket-- + "张票,还有" + ticket + "张票");
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
