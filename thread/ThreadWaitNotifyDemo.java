7e2f127674a309de1d884625549c7375cbde9e98
刘新宇 <lxy980504@foxmail.com>
2019-02-19 22:11:12 +0800

    sychronized 和lock 的用法以及condition与thread中的wait和NotifyAll

--- thread/ThreadWaitNotifyDemo.java
+++ thread/ThreadWaitNotifyDemo.java
@@ -4,8 +4,12 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * @author Lxy
+ */
 public class ThreadWaitNotifyDemo {
-    //    题目：现在两个线程，可以操作初始值为零的一个变量，实现一个线程对该变量加1，一个线程对该变量减1，
+
+//    题目：现在两个线程，可以操作初始值为零的一个变量，实现一个线程对该变量加1，一个线程对该变量减1，
 //            * 实现交替，来10轮，变量初始值为零。
 //            *
 //            * 1 高内聚低耦合前提下，线程   操作      资源类
